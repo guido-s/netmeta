3724ca8a5e5da2aa0f4711ea437c3dd91de51e9f
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2018-09-11 16:58:54 +0200

    netgraph(): maximum number of treatments increased from 26 to 626 if argument seq = 'optimal'

--- R/optcircle.R
+++ R/optcircle.R
@@ -40,7 +40,18 @@ optcircle <- function(x, start.layout = "eigen") {
   ##
   ## Distance matrix D of n-circle, here used to penalize distances
   ##
-  D <- t(P) %*% netconnection(LETTERS[1:n], LETTERS[c(2:n, 1)])$D.matrix %*% P
+  if (n > 676)
+    stop("Maximum number of 676 treatments", call. = FALSE)
+  else if (n > 26) {
+    trts <- paste0(rep(LETTERS[seq_len(n %/% 26)], rep(26, n %/% 26)), LETTERS)
+    if (n %% 26)
+      trts <- c(trts,
+                paste0(LETTERS[max(n %/% 26) + 1], LETTERS[seq_len(n %% 26)]))
+  }
+  else
+    trts <- LETTERS[1:n]
+  ##
+  D <- t(P) %*% netconnection(trts, trts[c(2:n, 1)])$D.matrix %*% P
   ##
   A.matrix <- sign(x$A.matrix) * D # A.matrix is the adjacency matrix, edges penalized
   ##
