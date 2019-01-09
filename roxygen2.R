dcf2c9fd3b35aad6915bf9f92685ed39c595832e
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2019-01-09 20:06:27 +0100

    Start roxygen commands in package directory

--- roxygen2.R
+++ roxygen2.R
@@ -8,24 +8,24 @@ library(roxygen2)
 ##
 ## (2) Create documentation file(s) in subdirectory testroxygen/man
 ##
-# roxygenise("testroxygen") # Only consider files in subdirectory testroxygen/R
-document("netmeta") # Also considers datasets in subdirectory netmeta/data
+# roxygenise("../testroxygen") # Only consider files in subdirectory testroxygen/R
+document("../netmeta") # Also considers datasets in subdirectory netmeta/data
 
 
 ##
 ## (3) Build R package and PDF file with help pages
 ##
-build("netmeta")
-build_manual("netmeta")
+build("../netmeta")
+build_manual("../netmeta")
 
 
 ##
 ## (4) Install R package
 ##
-install("netmeta")
+install("../netmeta")
 
 
 ##
 ## (5) Check R package
 ##
-check("netmeta")
+check("../netmeta")
