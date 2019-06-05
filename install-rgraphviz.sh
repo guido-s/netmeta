a31367b1c9262458b4e910f71d8f7ae19ac9cfba
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2019-06-05 13:04:05 +0200

    More changes to Rgraphviz script

--- install-rgraphviz.sh
+++ install-rgraphviz.sh
@@ -1,1 +1,1 @@
-Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager"); BiocManager::install("Rgraphviz")'
+Rscript -e 'if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager"); BiocManager::install(); BiocManager::install("Rgraphviz")'
